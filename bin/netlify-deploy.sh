0f6f8c335f4f48238c35014a21d156bb27e0aca6
Alex Hayton <alex.hayton@hopin.to>
2021-09-09 16:48:20 +0100

    Add the preview app URL to the build output!

--- bin/netlify-deploy.sh
+++ bin/netlify-deploy.sh
@@ -2,7 +2,8 @@
 
 npx netlify deploy >deploy.log
 export DEPLOY_URL=$(cat deploy.log | awk -F"[ ]+" '/Website Draft URL:/{print $4}')
+echo "🚀 Deployed a preview app to: ${DEPLOY_URL}"
 
 if [ -z ${CIRCLE_PR_NUMBER+x} ]; then
-    npx @humanwhocodes/github-comment alexhayton/svelte#${CIRCLE_PR_NUMBER} "🚀 Deployed a preview app to: ${DEPLOY_URL}"
+    npx @humanwhocodes/github-comment alexhayton/svelte#${CIRCLE_PULL_REQUEST##*/} "🚀 Deployed a preview app to: ${DEPLOY_URL}"
 fi
