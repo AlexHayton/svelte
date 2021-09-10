af8cdb7d7fcabbcf6c89b2a72bf560317a17cea5
Alex Hayton <alex.hayton@hopin.to>
2021-09-10 11:21:42 +0100

    Remove ANSI colour characters from the PR message

--- bin/netlify-deploy.sh
+++ bin/netlify-deploy.sh
@@ -2,8 +2,10 @@
 
 npx netlify deploy >deploy.log
 export DEPLOY_URL=$(cat deploy.log | awk -F"[ ]+" '/Website Draft URL:/{print $4}')
-echo "🚀 Deployed a preview app to: ${DEPLOY_URL}"
+# Remove ANSI colours from the URL
+export DEPLOY_MESSAGE="🚀 Deployed a preview app to: $(echo ${DEPLOY_URL} | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')"
 
+echo ${DEPLOY_MESSAGE}
 if [ -z ${CIRCLE_PR_NUMBER+x} ]; then
-    gh pr comment --body "🚀 Deployed a preview app to: ${DEPLOY_URL}"
+    gh pr comment --body ${DEPLOY_MESSAGE}
 fi
