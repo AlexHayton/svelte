b1996df4959bebf61b86bea9dcbd9837c1f89a9f
Alex Hayton <alex.hayton@gmail.com>
2021-09-10 11:46:00 +0100

    Use npx (#1)
    * Use npx
    
    * Add the preview app URL to the build output!
    
    * Use GH CLI
    
    * Fix the orb
    
    * Remove ANSI colour characters from the PR message
    
    * Tweak the build script even more
    
    * Use CIRCLE_PULL_REQUEST

--- bin/netlify-deploy.sh
+++ bin/netlify-deploy.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-netlify deploy >deploy.log
-export DEPLOY_URL=$(cat deploy.log | awk -F"[ ]+" '/Website Draft URL:/{print $4}')
-
 if [ -z ${CIRCLE_PR_NUMBER+x} ]; then
-    npx @humanwhocodes/github-comment alexhayton/svelte#${CIRCLE_PR_NUMBER} "🚀 Deployed a preview app to: ${DEPLOY_URL}"
+    export DEPLOY_ALIAS=$(git rev-parse --short HEAD)
+else
+    export DEPLOY_ALIAS="pr-$CIRCLE_PR_NUMBER"
+fi
+
+npx netlify deploy --alias ${DEPLOY_ALIAS} >deploy.log
+
+# Extract the deploy URL from the netlify output
+export DEPLOY_URL=$(cat deploy.log | awk -F"[ ]+" '/Website Draft URL:/{print $4}')
+# Remove ANSI colours from the URL
+export DEPLOY_MESSAGE="🚀 Deployed a preview app to: $(echo ${DEPLOY_URL} | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')"
+
+echo "${DEPLOY_MESSAGE}"
+if [ ! -z ${CIRCLE_PULL_REQUEST+x} ]; then
+    echo "Posting a github comment to the pull request..."
+    gh pr comment --body "${DEPLOY_MESSAGE}"
 fi
